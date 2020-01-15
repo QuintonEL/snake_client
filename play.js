1eddbdb705329f9d53e6d19f00152f16c7ca4128
QuintonEL <quintonlaborde@gmail.com>
2020-01-15 19:51:32 +0000

    file to call the connection function

--- play.js
+++ play.js
@@ -1,24 +1,4 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-
-  // client.js
-conn.on('data', (data) => {
-  console.log('Server says: ', data);
-});
-
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  return conn;
-}
-
+// play.js
+const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
